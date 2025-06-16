8923e2a37ec9c4c502d8194866f2d7dbf78f0b60
Mheteula <rushalmanandhar@gmail.com>
2025-06-16 16:52:30 +0545

    phase1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-function App() {
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+function App() {
   return (
     <>
-    <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <ToastContainer/>
+      <main className="py-3">
+        <Outlet/>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
