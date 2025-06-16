b879a757b4d43e00aabfb4bc7dac050e807204cd
Mheteula <rushalmanandhar@gmail.com>
2025-06-16 14:10:17 +0545

    backend

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+//packages
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+//utils
+import connectDB from "./config/db.js";
+
+//app using
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//host port
+dotenv.config();
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server running in ${port}`));
+
+connectDB();
