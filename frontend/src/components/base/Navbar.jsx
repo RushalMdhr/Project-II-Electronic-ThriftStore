dcf92061da50b0d2e3587a7ea56bc107143c3b37
xeniumhs <thexenium07@gmail.com>
2025-06-27 23:11:41 +0545

    Added admin panel link in navbar for admin

--- frontend/src/components/base/Navbar.jsx
+++ frontend/src/components/base/Navbar.jsx
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import "remixicon/fonts/remixicon.css";
 import { useLogoutMutation } from "../../redux/api/usersApiSlice";
 import { logout } from "../../redux/features/auth/authSlice";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { userInfo } = useSelector((state) => state.auth);
@@ -55,6 +56,16 @@ const Navbar = () => {
           >
             <i className="ri-apps-2-line mr-1"></i> Categories
           </a>
+
+          {userInfo?.isAdmin && (
+            <Link
+              to="/admin"
+              className="flex items-center text-white text-lg font-semibold hover:text-[#1de9b6] transition"
+            >
+              <i className="ri-shield-user-line mr-1"></i> Admin Panel
+            </Link>
+          )}
+
           {userInfo && userInfo.isVendor ? (
             <>
               <a
