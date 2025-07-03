bdcc8af6750f7ae57e0b17b78d92ba8219254611
xeniumhs <thexenium07@gmail.com>
2025-07-03 19:03:51 +0545

    fixed routes with authenticate to disallow banned users from using cart and category routes

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { getAllCategories, createCategory, updateCategory,deleteCategory } from '../controllers/categoryControllers.js';
+import { authenticate } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
-router.route("/").get(getAllCategories).post(createCategory);
+router.route("/").get(authenticate, getAllCategories).post(authenticate, createCategory);
 
-router.route("/:id").put(updateCategory).delete(deleteCategory);
-
+router.route("/:id").put(authenticate, updateCategory).delete(authenticate, deleteCategory);
 
 export default router;
