66d1cd7ddbd4cfaef98c5384079e77d7c3fe0633
xeniumhs <thexenium07@gmail.com>
2025-07-31 19:49:50 +0545

    Updated category model

--- backend/models/categoryModel.js
+++ backend/models/categoryModel.js
@@ -1,17 +1,24 @@
 import mongoose from "mongoose";
 
-const categorySchema = mongoose.Schema({
-    name :{
-        type:String,
-        trim:true,
-        required:true,
-        maxlength:32,
-        unique:true
+const categorySchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+      unique: true,
     },
-    used : {
-        type:Number,
-        default:0
-    }
-})
+    used: {
+      type: Number,
+      default: 0,
+    },
+    image: {
+      type: String,
+      default: "https://via.placeholder.com/150",
+    },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("Category",categorySchema)
+export default mongoose.model("Category", categorySchema);
