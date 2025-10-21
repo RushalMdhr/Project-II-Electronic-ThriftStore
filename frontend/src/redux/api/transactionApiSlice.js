deedbe5143259c8595cdd4ce707e846e56c1bfbf
Rushal <rushalmanandhar@gmail.com>
2025-10-21 11:16:46 +0545

    transaction api slice

--- frontend/src/redux/api/transactionApiSlice.js
+++ frontend/src/redux/api/transactionApiSlice.js
@@ -3,16 +3,22 @@ import { apiSlice } from "./apiSlice";
 
 export const transactionApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    esewaPayment : builder.mutation({
-        query : (data)=>({
-            url : `${ESEWA_URL}/initiate-payment`,
-            method : "POST",
-            body : data
-        }),
-    })
+    esewaPayment: builder.mutation({
+      query: (data) => ({
+        url: `${ESEWA_URL}/initiate-payment`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    esewaSuccess: builder.mutation({
+      query: ({product_id}) => ({
+        url: `${ESEWA_URL}/payment-status`,
+        method: "POST",
+        body : {product_id},
+      }),
+    }),
   }),
 });
 
-export const {
-    useEsewaPaymentMutation
-}=transactionApiSlice
+export const { useEsewaPaymentMutation, useEsewaSuccessMutation } = transactionApiSlice;
