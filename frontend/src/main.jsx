c788efa667e5bcd08f842bd65f2124e6012b252d
Rushal <rushalmanandhar@gmail.com>
2025-11-14 10:23:13 +0545

    from frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -114,7 +114,7 @@ const router = createBrowserRouter(
       {/* ________________________________ LOGGED IN ________________________________________*/}
 
       <Route path="/" element={<PrivateRoute />}>
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/:name" element={<Profile />} />
         <Route path="/myorders" element={<MyOrders />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/checkout" element={<Checkout />} />
