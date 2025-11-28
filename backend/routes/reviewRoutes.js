f5d49ffe98032564b3715a21fec813ed856e8edf
xeniumhs <thexenium07@gmail.com>
2025-11-28 22:36:53 +0545

    review routes

---
+++ backend/routes/reviewRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import {
+  createReview,
+  getReviewsByProduct,
+  getReviewsBySeller,
+  deleteReview,
+} from "../controllers/reviewController.js";
+import { authenticate } from "../middlewares/authMiddleware.js";
+import { uploadSingle } from "../middlewares/multer.js";
+const router = express.Router();
+
+// Create review
+router.post("/", authenticate, uploadSingle, createReview);
+
+// Get reviews of a product
+router.get("/product/:productId", getReviewsByProduct);
+
+// Get reviews of a seller
+router.get("/seller/:sellerId", getReviewsBySeller);
+
+// Delete review
+router.delete("/:id", authenticate, deleteReview);
+
+export default router;
