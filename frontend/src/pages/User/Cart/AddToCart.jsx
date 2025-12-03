127924b98e03d3a95119202e61e9407ad24c0c0e
kriti0317 <kritikoju2062@gmail.com>
2025-12-03 23:07:03 +0545

    added toast message

--- frontend/src/pages/User/Cart/AddToCart.jsx
+++ frontend/src/pages/User/Cart/AddToCart.jsx
@@ -22,8 +22,8 @@ const AddToCart = ({ productId, disabled }) => {
       await addToCart({ userId, productId, quantity: 1 }).unwrap();
       alert("✅ Product added to cart!");
     } catch (error) {
-      console.error("❌ Failed to add to cart:", error);
-      alert("❌ Failed to add to cart.");
+      console.error("❌ Fail to add to cart:", error);
+      toast.error(error?.data?.message || "Failed to add to cart");
     }
   };
 
