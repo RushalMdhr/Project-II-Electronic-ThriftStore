e593573043b73b92657e237db8d37961080b8fe6
xeniumhs <thexenium07@gmail.com>
2025-12-03 18:02:22 +0545

    remove buy now

--- frontend/src/pages/Home/ProductTools/ProductOverview.jsx
+++ frontend/src/pages/Home/ProductTools/ProductOverview.jsx
@@ -248,17 +248,7 @@ const ProductOverview = () => {
                     />
                   </div>
 
-                  {/* Buy Now Button Wrapper */}
-                  <div className="flex-0.3">
-                    <button
-                      type="button"
-                      disabled={!product.countInStock}
-                      className="mt-2 w-full px-4 py-2 rounded-lg font-medium transition-all duration-200 
-                 bg-amber-500 text-white shadow hover:bg-amber-600 disabled:opacity-50 disabled:cursor-not-allowed"
-                    >
-                      Buy Now
-                    </button>
-                  </div>
+                  
                 </div>
               )}
               {(!userInfo ||
